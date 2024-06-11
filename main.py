6e230a3c7f84a32cd7629ebe0aca9c960dc2a769
Yorgo Kennos <yorgokennos@gmail.com>
2024-06-10 21:26:47 -0400

    refactored the drawing commands/positions of the chess pieces

--- main.py
+++ main.py
@@ -60,6 +60,44 @@ for key in pieces:
 #offset based on square size and piece size
 offset = (square_size - piece_size) // 2
 
+
+#list of rook positions
+rook_positions = [
+    ('rook_b', (0, 0)),
+    ('rook_b', (7, 0)),
+    ('rook_w', (0, 7)),
+    ('rook_w', (7, 7))
+]
+
+#list of knight positions
+knight_positions = [
+    ('knight_b', (1, 0)),
+    ('knight_b', (6, 0)),
+    ('knight_w', (1, 7)),
+    ('knight_w', (6, 7))
+]
+
+#list of bishop positions
+bishop_positions = [
+    ('bishop_b', (2, 0)),
+    ('bishop_b', (5, 0)),
+    ('bishop_w', (2, 7)),
+    ('bishop_w', (5, 7))
+]
+
+# list of queen positions
+queen_positions = [
+    ('queen_b', (4, 0)),
+    ('queen_w', (4, 7))
+]
+
+# list of king positions
+king_positions = [
+    ('king_b', (3, 0)),
+    ('king_w', (3, 7))
+]
+
+
 #draw each black pawn
 for i in range(8):
     surface.blit(pieces['pawn_b'], (i * square_size + offset, 1 * square_size + offset))
@@ -69,38 +107,28 @@ for i in range(8):
     surface.blit(pieces['pawn_w'], (i * square_size + offset, 6 * square_size + offset))
 
 #draw each rook
-surface.blit(pieces['rook_b'], (0 * square_size + offset, 0 * square_size + offset))
-surface.blit(pieces['rook_b'], (7 * square_size + offset, 0 + offset))
-surface.blit(pieces['rook_w'], (0 * square_size + offset,7 * square_size + offset))
-surface.blit(pieces['rook_w'], (7 * square_size + offset, 7 * square_size + offset ))
+for piece, (x, y) in rook_positions:
+    surface.blit(pieces[piece], (x * square_size + offset, y * square_size + offset))
 
 #draw each knight
-surface.blit(pieces['knight_b'], (1 * square_size + offset, 0 * square_size + offset))
-surface.blit(pieces['knight_b'], (6 * square_size + offset, 0 + offset))
-surface.blit(pieces['knight_w'], (1 * square_size + offset, 7 * square_size + offset))
-surface.blit(pieces['knight_w'], (6 * square_size + offset, 7 * square_size + offset ))
+for piece, (x, y) in knight_positions:
+    surface.blit(pieces[piece],(x * square_size + offset, y * square_size + offset))
 
 #draw each bishop
-surface.blit(pieces['bishop_b'], (2 * square_size + offset, 0 * square_size + offset))
-surface.blit(pieces['bishop_b'], (5 * square_size + offset, 0 * square_size + offset))
-surface.blit(pieces['bishop_w'], (2 * square_size + offset, 7 * square_size + offset))
-surface.blit(pieces['bishop_w'], (5 * square_size + offset, 7 * square_size + offset))
+for piece, (x, y) in bishop_positions:
+    surface.blit(pieces[piece], (x * square_size + offset, y * square_size + offset))
 
 #draw both queens
-surface.blit(pieces['queen_b'], (4 * square_size + offset, 0 * square_size + offset))
-surface.blit(pieces['queen_w'], (4 * square_size + offset, 7 * square_size + offset))
-
+for piece, (x, y) in queen_positions:
+    surface.blit(pieces[piece], (x * square_size + offset, y * square_size + offset))
 
 #draw both kings
-surface.blit(pieces['king_b'], (3 * square_size + offset, 0 * square_size + offset))
-surface.blit(pieces['king_w'], (3 * square_size + offset, 7 * square_size + offset))
+for piece, (x, y) in king_positions:
+    surface.blit(pieces[piece], (x * square_size + offset, y * square_size + offset))
 
 
 #"Render" drawings?????? CHECK THIS VERBAGE
 pygame.display.flip()
-
-
-
 
 while running:
     # poll for events
